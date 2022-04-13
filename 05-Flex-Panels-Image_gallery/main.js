a2b2e28bc28075ccc4883268fcb2feb992cb7fb8
David Flanik <daveflanik@gmail.com>
2022-04-13 21:04:26 +0900

    work on flex-box

---
+++ 05-Flex-Panels-Image_gallery/main.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if(e.propertyName.includes('flex')) { // this is just to ensure that there are no issues with browsers. It will toggle active if flex is included in panel
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
